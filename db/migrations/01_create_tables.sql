322ab44c8e72d40ed5ceec6dea907dcfa6733c57
Kintaro-Oe <marcusclairmont@gmail.com>
2019-05-10 13:28:10 +0100

    Travis - remove default value from table creation

--- db/migrations/01_create_tables.sql
+++ db/migrations/01_create_tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (id serial PRIMARY KEY, first_name VARCHAR (30), surname VARC
 CREATE TABLE properties (id serial PRIMARY KEY, user_id VARCHAR, property_name VARCHAR (60), price DECIMAL (6,2), available_dates DATE, description VARCHAR (240));
 CREATE TABLE user_requests (user_request_id serial PRIMARY KEY, user_id VARCHAR);
 CREATE TABLE property_requests (property_request_id serial PRIMARY KEY, property_id VARCHAR);
-CREATE TABLE requests (id serial PRIMARY KEY, user_request_id INTEGER REFERENCES user_requests(user_request_id), property_id INTEGER REFERENCES property_requests(property_request_id), status VARCHAR (30) DEFAULT 'pending');
+CREATE TABLE requests (id serial PRIMARY KEY, user_request_id INTEGER REFERENCES user_requests(user_request_id), property_id INTEGER REFERENCES property_requests(property_request_id), status VARCHAR (30));
 
 CREATE DATABASE bnb_test;
 \c bnb_test;
@@ -12,4 +12,4 @@ CREATE TABLE users (id serial PRIMARY KEY, first_name VARCHAR (30), surname VARC
 CREATE TABLE properties (id serial PRIMARY KEY, user_id VARCHAR, property_name VARCHAR (60), price DECIMAL (6,2), available_dates DATE, description VARCHAR (240));
 CREATE TABLE user_requests (user_request_id serial PRIMARY KEY, user_id VARCHAR);
 CREATE TABLE property_requests (property_request_id serial PRIMARY KEY, property_id VARCHAR);
-CREATE TABLE requests (id serial PRIMARY KEY, user_request_id INTEGER REFERENCES user_requests(user_request_id), property_id INTEGER REFERENCES property_requests(property_request_id), status VARCHAR (30) DEFAULT 'pending');
+CREATE TABLE requests (id serial PRIMARY KEY, user_request_id INTEGER REFERENCES user_requests(user_request_id), property_id INTEGER REFERENCES property_requests(property_request_id), status VARCHAR (30));
